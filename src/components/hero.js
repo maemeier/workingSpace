531fce372114710047db287348d30b55630ebc94
Mae Kangsadan <maemeier@live.com>
2020-01-02 01:12:22 +0100

    header

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-      <img className="hero" src="../img/hero.jpg" alt="hero" />
+      <img className="hero" src="../img/place2.jpg" alt="hero" />
       <h1 className="heroTitle">COWORKING SPACE</h1>
       <h2 className="heroTitle2">Find office near you</h2>
     </div>
